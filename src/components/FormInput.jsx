3c8265a39474b7f9b271524b406ea39ac7c8a3ef
Pablen Israelsky <pabloisraelsky@gmail.com>
2024-09-27 15:49:55 -0300

    Agregamos estilos y los iconos a cada input

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
+import '../index.css'
 
 const FormInput = ({ label, type, name, placeholder, icon }) => {
   return (
-    <div className="form-group">
-      <label>{label}</label>
-      <div className="input-icon">
-        <i className={`icon-${icon}`} />
-        <input
-          type={type}
-          name={name}
-          placeholder={placeholder}
-          required
-          icon={icon}
+    <div className="form-input">
+      <label htmlFor={name}>{label}</label>
+      <div className="input-container">
+        <i className={`fa fa-${icon}`} style={{ marginRight: '8px' }}></i> {/* Icono */}
+        <input 
+          type={type} 
+          name={name} 
+          placeholder={placeholder} 
         />
       </div>
     </div>
   );
 };
+
 
 export default FormInput;
--- src/components/FormRegister.jsx
+++ src/components/FormRegister.jsx
@@ -1,62 +1,80 @@
 import React from 'react';
+import { Stack, Box } from '@mui/material';
 import FormInput from './FormInput';
 import RegisterButton from './ButtonRegister';
 
-export const FormRegister = () => {
-  // Función que vamos a ejecutar al hacer click en el boton
+
+export const FormRegistro = () => {
   const handleRegisterClick = () => {
     console.log("Formulario enviado!");
-    // Aca vamos a sumamos la logica del boton
   };
 
+
   return (
-    <form className="register-form">
-      <div className="form-row">
-        <FormInput 
-          label="Nombre" 
-          type="text" 
-          name="nombre" 
-          placeholder="Ingresa tu nombre" 
+    <Box className='form-container'
+    component="form"
+    oValidate
+    autoComplete="off">
+      <Stack direction="row" spacing={5} mt={2}>
+        <FormInput
+          label="Nombre"
+          type="text"
+          name="nombre"
+          placeholder="Ingresa tu nombre"
+          icon="user"
         />
-        <FormInput 
-          label="Apellido" 
-          type="text" 
-          name="apellido" 
-          placeholder="Ingrese tu apellido" 
+        <FormInput
+          label="Apellido"
+          type="text"
+          name="apellido"
+          placeholder="Ingresa tu apellido"
+          icon="user"
         />
-      </div>
-      <div className="form-row">
-        <FormInput 
-          label="DNI" 
-          type="text" 
-          name="dni" 
-          placeholder="Ingresa tu DNI" 
+      </Stack>
+
+
+      <Stack direction="row" spacing={5} mt={2}>
+        <FormInput
+          label="DNI"
+          type="text"
+          name="dni"
+          placeholder="Ingresa tu DNI"
+          icon="address-card"
         />
-        <FormInput 
-          label="Email" 
-          type="email" 
-          name="email" 
-          placeholder="Ingresa tu email" 
+        <FormInput
+          label="Email"
+          type="email"
+          name="email"
+          placeholder="Ingresa tu email"
+          icon="envelope"
         />
-      </div>
-      <div className="form-row">
-        <FormInput 
-          label="Contraseña" 
-          type="password" 
-          name="password" 
-          placeholder="Ingresa tu contraseña" 
+      </Stack>
+
+
+      <Stack direction="row" spacing={5} mt={2}>
+        <FormInput
+          label="Contraseña"
+          type="password"
+          name="password"
+          placeholder="Ingresa tu contraseña"
+          icon="lock"
         />
-        <FormInput 
-          label="Confirmar contraseña" 
-          type="password" 
-          name="confirm_password" 
-          placeholder="Confirma tu contraseña" 
+        <FormInput
+          label="Confirmar Contraseña"
+          type="password"
+          name="confirm_password"
+          placeholder="Confirmar contraseña"
+          icon="lock"
         />
-      </div>
-      <RegisterButton 
-        text="Registrarse" 
-        onClick={handleRegisterClick} 
-      />
-    </form>
+      </Stack>
+
+
+      <Stack mt={3} direction="row" justifyContent="center">
+        <RegisterButton
+          text="Registrarse"
+          onClick={handleRegisterClick}
+        />
+      </Stack>
+    </Box>
   );
 };
