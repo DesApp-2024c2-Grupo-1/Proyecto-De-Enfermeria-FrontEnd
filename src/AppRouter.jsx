69d61841bf3d36d2e27865342c633dfdaebd22d3
maia-ab <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-10-20 17:18:14 -0300

    actualizacion de las rutas

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -4,17 +4,16 @@ import { FilmsPage } from "./pages/FilmsPage";
 import { ActorsPage } from "./pages/ActorsPage";
 import { Box } from "@mui/material";
 import { grey } from "@mui/material/colors";
+import { PerfilDocentePage } from "./pages/PerfilDocentePage";
+import { HomePage } from "./pages/HomePage";
 
 export function AppRouter() {
   return (
     <Routes>
-      <Route path='/films' element={<FilmsPage />} />
-      <Route path='/actors' element={<ActorsPage />} />
-      <Route path='/' element={
-        <Box sx={{ typography: 'h5', color: grey[900] }}>
-          Elija una opción del menú
-        </Box>
-      } />
+      <Route path="/films" element={<FilmsPage />} />
+      <Route path="/actors" element={<ActorsPage />} />
+      <Route path="/perfilDocente" element={<PerfilDocentePage />} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
   );
 }
---
+++ src/pages/PerfilDocentePage.jsx
@@ -0,0 +1,12 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { Box, Card, CardMedia, Grid, Stack, Typography } from "@mui/material";
+import { getAllActors } from "../services/ActorsService";
+
+
+export function PerfilDocentePage() {
+
+
+  return <>
+    <div>Perfil Docente</div>
+  </>  
+}
