2d47961e2ad8cbac2fa0c4a884f22e1af0c9118e
maia-ab <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-10-21 00:44:34 -0300

    agregue iconos y logo

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -1,7 +1,11 @@
 import { Box, Stack } from "@mui/material";
 import { useLocation, useNavigate } from "react-router-dom";
+import HomeIcon from "/assets/home.png";
+import ProfileIcon from "/assets/profile.png";
+import AlumnosIcon from "/assets/alumnos.png";
 
-function MenuOption({ path, label }) {
+
+function MenuOption({ path, label, icon }) {
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
@@ -9,10 +13,16 @@ function MenuOption({ path, label }) {
     <Box
       sx={{
         typography: pathname === path ? "topMenu" : "topMenuSelected",
-        padding: 2,
+        padding: 8,
+        cursor: "pointer",
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+       
       }}
       onClick={() => navigate(path)}
     >
+      <img src={icon} alt={label} style={{ width: 30, height: 30 }} />
       {label}
     </Box>
   );
@@ -21,10 +31,20 @@ function MenuOption({ path, label }) {
 export function Menu() {
   return (
     <div>
-      <Stack direction="row" alignItems="center" className="menu">
-        <MenuOption path="/home" label="Inicio" />
-        <MenuOption path="/alumnos" label="Alumnos" />
-        <MenuOption path="/perfilDocente" label="Perfil" />
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+        className="menu"
+      >
+        <Stack direction="row" spacing={2}>
+          <MenuOption path="/home" label="Inicio" icon={HomeIcon} />
+          <MenuOption path="/alumnos" label="Alumnos" icon={AlumnosIcon} />
+          <MenuOption path="/perfilDocente" label="Perfil" icon={ProfileIcon} />
+        </Stack>
+
+        {/* Logo a la derecha */}
+        <img src="../assets/logoUnahur.svg" className="logo" alt="Logo" />
       </Stack>
     </div>
   );
