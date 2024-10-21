90f1d11afb762a49fd6969150e31d44efc74e94f
maia-ab <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-10-21 02:28:02 -0300

    arme componente carpeta

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,40 @@
+import { Grid } from "@mui/material";
+import Carpeta from "../components/Carpeta";
+
 export function HomePage() {
+  const carpetas = [
+    "Carpeta 1",
+    "Carpeta 2",
+    "Carpeta 3",
+    "Carpeta 4",
+    "Carpeta 5",
+    "Carpeta 6",
+    "Carpeta 7",
+    "Carpeta 8",
+    "Carpeta 9",
+    "Carpeta 10",
+    "Carpeta 11",
+    "Carpeta 12"
+  ];
+
   return (
-    <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
       <h1>¡Bienvenido/a, Carlos Perez!</h1>
+
+      <Grid container spacing={5}>
+        {carpetas.map((titulo, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Carpeta titulo={titulo} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }
