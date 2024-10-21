3a1351a32f93adefcf467c0ab33a1dd0f8ff1a2f
maia-ab <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-10-20 21:50:09 -0300

    agregue inputs a login

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,9 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import { FilmsPage } from "./pages/FilmsPage";
-// import { FilmsPage } from "./pages/FilmsPageReduxStyle";
-import { ActorsPage } from "./pages/ActorsPage";
-import { Box } from "@mui/material";
-import { grey } from "@mui/material/colors";
 import { PerfilDocentePage } from "./pages/PerfilDocentePage";
 import { HomePage } from "./pages/HomePage";
 import { LoginPage } from "./pages/LoginPage";
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
 
 export function LoginPage() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = () => {
-    console.log("Login");
+    console.log("Login Attempt", { email, password });
     navigate("home");
   };
 
@@ -16,6 +19,21 @@ export function LoginPage() {
   return (
     <>
       <div>Login</div>
+      <input
+        type="email"
+        placeholder="nombre@apellido.com"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="input-email"
+      />
+
+      <input
+        type="password"
+        placeholder="contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="input-password"
+      />
       <Button text="Iniciar sesión" onClick={handleLogin} className="login" />
       <p>¿No tenés cuenta?</p>
       <Button text="Registrate" onClick={handleRegister} />
