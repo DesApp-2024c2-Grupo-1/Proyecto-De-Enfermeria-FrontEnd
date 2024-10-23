200f2387eae953f99590662f4d20258a79a86d8a
maia-ab <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-10-23 00:52:18 -0300

    logica de lista de preguntas regsitradas

--- src/components/ListaPreguntas.jsx
+++ src/components/ListaPreguntas.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Pregunta } from "../components/Pregunta";
 
-export function ListaPreguntas({ preguntas, disabled}) {
-  
+export function ListaPreguntas({ preguntas, disabled }) {
   const [respuestas, setRespuestas] = useState(
     preguntas.map((pregunta) => pregunta.respuesta || null)
   );
@@ -16,25 +15,29 @@ export function ListaPreguntas({ preguntas, disabled}) {
   };
 
   const handleOnClick = () => {
-    setRegistrado(true);
+    setRegistrado(!registrado);
+    console.log(respuestas);
   };
 
   return (
     <div>
-    <div className="listaPreguntas">
-      {preguntas.map((pregunta, index) => (
-        <Pregunta
-          key={index}
-          pregunta={pregunta.pregunta}
-          respuesta={respuestas[index]} 
-          disabled={disabled || pregunta.respuesta !== undefined} 
-          onChange={(nuevaRespuesta) => handleRespuestaChange(index, nuevaRespuesta)} 
-        />
-      ))}
+
+      <div className="listaPreguntas">
+        {preguntas.map((pregunta, index) => (
+          <Pregunta
+            key={index}
+            pregunta={pregunta.pregunta}
+            respuesta={respuestas[index]}
+            disabled={registrado || pregunta.respuesta !== undefined}
+            onChange={(nuevaRespuesta) =>
+              handleRespuestaChange(index, nuevaRespuesta)
+            }
+          />
+        ))}
+        {!registrado ? <button onClick={handleOnClick}> Registrar </button> : <div></div>}
+        
+      </div>
     </div>
-    <div>
-      <button></button>
-    </div>
-    </div>
+    
   );
 }
