f06920ac4a49e9f84768e65ba1ab87c04fd77e54
Pablen Israelsky <pabloisraelsky@gmail.com>
2024-10-30 16:52:56 -0300

    Solucionado un error en la ruta

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -24,9 +24,7 @@ export function RegisterPage() {
 
     const docenteData = { nombre, apellido, email, dni, password };
     registrarDocente(docenteData);
-    navigate("home");
-
-    
+    navigate("/home");
   };
  
   
