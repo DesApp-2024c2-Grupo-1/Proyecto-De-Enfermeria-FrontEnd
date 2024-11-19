d7b9e02e729ba0799d6288b04cd839281d4df5e2
Pablen Israelsky <pabloisraelsky@gmail.com>
2024-11-19 18:28:20 -0300

    Agregamos buscar docente por email

--- src/services/DocenteService.js
+++ src/services/DocenteService.js
@@ -4,3 +4,8 @@ export const registrarDocente = async (docenteData) => {
     const response = await axios.post('http://localhost:3000/docente', docenteData);
     return response.data;
   };
+
+export const getDocenteByEmail = async (email) => {
+  const response = await axios.get(`http://localhost:3000/alumno/dni/${email}`)
+  return response.data
+}
