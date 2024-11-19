618472a559175b9167f842cd2149e184b66546c1
AdryPika <adriana.picallo@estudiantes.unahur.edu.ar>
2024-11-19 19:34:25 -0300

    Redirección registro docente

--- src/pages/RegistroAlumnoExitoso.jsx
+++ src/pages/RegistroAlumnoExitoso.jsx
@@ -9,7 +9,7 @@ export function RegistroAlumnoExitoso() {
         <img src="../assets/profile.png" className="bordePerfil"/>
         <div className="recuadroTexto">
         <h1>¡Alumno registrado con éxito!</h1>
-        <p>¡Bienvenid@, muchas gracias por registrarte!</p>
+        <p id="espacio">¡Bienvenid@, muchas gracias por registrarte!</p>
         </div>
         <div><img src="../assets/unahur-logo-figma-sf.png" className="unahur-logo" alt="Logo UNAHUR"/> </div>
         </div>
--- src/pages/RegistroDocenteExitoso.jsx
+++ src/pages/RegistroDocenteExitoso.jsx
@@ -6,15 +6,15 @@ export function RegistroDocenteExitoso() {
   const navigate = useNavigate();
 
   const handleRedirect = () => {
-    navigate('/home');               //seria para volver al inicio...home o perfil docente
+    navigate('/');               
   }
     return (
         <>
         <div className="alineacion">
         <img src="../assets/profile.png" className="bordePerfil"/>
-        <div className="recuadroTexto">
+        <div className="recuadroTexto1">
         <h1>¡Docente registrado con éxito!</h1>
-        <button className="botonClaro" onClick={handleRedirect}>Volver al Inicio</button>
+        <button className="botonClaro" onClick={handleRedirect}>Iniciar sesión</button>
         </div>
         <div><img src="../assets/unahur-logo-figma-sf.png" className="unahur-logo" alt="Logo UNAHUR"/> </div>
         </div>
