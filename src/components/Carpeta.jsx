45850298197c45bbd3e6d659dafc13cda0145f66
maia-ab <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2024-11-20 00:30:25 -0300

    modificar carpetas

--- src/components/Carpeta.jsx
+++ src/components/Carpeta.jsx
@@ -1,14 +1,51 @@
 import Button from "../components/Button";
+import { Box, Typography, Paper, Stack } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 function Carpeta({ titulo }) {
   const navigate = useNavigate();
+
   return (
-    <div className="carpeta">
-      <div className="carpetaFondo">
-        <div className="carpetaPestaña"></div>
-        <h2>{titulo}</h2>
-        <div className="botonesContainer">
+    <Stack
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Paper
+        sx={{
+          paddingTop: "5vh", /* Cambiar a 2.5  */
+          paddingBottom: "5vh",
+          width: "15rem",
+          backgroundColor: "#daf2e3",
+          position: "relative",
+          borderRadius: "5px",
+          boxShadow: "3px 3px 5px rgba(0, 0, 0, 0.3)",
+        }}
+      >
+        {/* CarpetaPestaña */}
+        <Box
+          sx={{
+            width: "80px",
+            height: "20px",
+            backgroundColor: "#daf2e3",
+            position: "absolute",
+            top: "-15px",
+            borderBottom: "none",
+            borderRadius: "5px 5px 0 0",
+          }}
+        />
+        {/* Contenedor de botones */}
+        <Stack
+          sx={{
+            flexDirection: "column",
+            justifyContent: "center", 
+            alignItems: "center", 
+            height: "100%", 
+            gap: "10px",
+          }}
+        >
           <Button
             text="Evaluar"
             className="botonClaro"
@@ -19,10 +56,29 @@ function Carpeta({ titulo }) {
             className="botonClaro"
             onClick={() => navigate("/registroEvaluaciones")}
           />
-        </div>
-      </div>
-    </div>
+          
+        </Stack>
+      </Paper>
+
+      {/* Título */}
+      <Typography
+        sx={{
+          fontSize: "1.3rem",
+
+          margin: "10px 0 0 0",
+          textAlign: "center",
+          width: "15rem",
+        }}
+      >
+        {titulo}
+      </Typography>
+    </Stack>
   );
 }
 
 export default Carpeta;
+
+/*
+
+
+*/
