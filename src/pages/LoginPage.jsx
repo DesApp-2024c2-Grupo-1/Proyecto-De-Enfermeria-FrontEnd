c9c96a7e3a40e77a9b3218e3464bfa30628a89f3
Pri-PJ <priscilasolange.jofregil@estudiantes.unahur.edu.ar>
2024-12-05 16:53:52 -0300

    Se arreglo los errores del login docente y agregamos manejo de errores con sus alerts

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,55 +1,43 @@
-import {  useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
-import FormInput from '../components/FormInput'
-import "../index.css"
+import FormInput from "../components/FormInput";
+import "../index.css";
 import { getDocenteByDni } from "../services/DocenteService";
 import { useDocente } from "../context/DocenteContext";
+import { Snackbar, Alert } from "@mui/material";
 
 export function LoginPage() {
   const navigate = useNavigate();
   const [dni, setDni] = useState("");
   const [password, setPassword] = useState("");
-  const { setDocenteContext } = useDocente()
-  const [loading, setLoading] = useState(false);  // Estado para carga
-  const [error, setError] = useState(""); // Estado para manejo de errores
+  const { setDocenteContext } = useDocente();
+  const [error, setError] = useState("");
+  const [openSnackbar, setOpenSnackbar] = useState(false);
 
-  const [docente, setDocente] = useState(null)
-  
+  const [docente, setDocente] = useState(null);
 
   const fetchDocente = async (dni) => {
-    try {
-      setLoading(true);
-      setError(""); // Resetear errores previos
-      const data = await getDocenteByDni(Number(dni));
-      setDocente(data);
-    } catch (err) {
-      setError("Docente no encontrado.");
-    }  finally {
-      setLoading(false);
-  }
+    const data = await getDocenteByDni(Number(dni));
+    setDocente(data);
   };
-  
+
   useEffect(() => {
-    if (dni.length === 8) { 
-      fetchDocente(dni);
-    }
-    }, [dni]);
+    fetchDocente(dni);
+  }, [dni]);
 
   const handleLogin = async () => {
     if (!docente) {
       setError("Docente no encontrado.");
-      return;
-    }
-
-    if (password === docente.password) {
-      setDocenteContext(docente); // Establecer docente en contexto
+      setOpenSnackbar(true);
+    } else if (password === docente.password) {
+      setDocenteContext(docente);
       navigate("home");
     } else {
-      setError("Contraseña incorrecta.");
+      setError("La contraseña es incorrecta");
+      setOpenSnackbar(true);
     }
   };
-
 
   const handleRegister = () => {
     navigate("register");
@@ -57,48 +45,80 @@ export function LoginPage() {
 
   return (
     <>
-    <div className="alineacion">
-    <img src="../assets/profile.png" className="bordePerfil"/> 
-      <div className="recuadroTexto">
-      <FormInput
-        placeholder="Ingrese su DNI"
-        value={dni}
-        onChange={(e) => setDni(e.target.value)}
-        className="recuadroInputs"
-        icono="address-card"
-      />
-      <FormInput
-        type="password"
-        placeholder="Contraseña"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        className="recuadroInputs"
-        icono="lock"
-      />
-      <Button
-        text={loading ? "Cargando..." : "Iniciar sesión"}
-        onClick={handleLogin}
-        className="botonClaro"
-        disabled={loading} // Deshabilitar botón mientras se carga
-      />
-      
-      {error && <p className="error-message">{error}</p>}  {/* Mostrar el mensaje de error */}
+      <div className="alineacion">
+        <img src="../assets/profile.png" className="bordePerfil" />
+        <div className="recuadroTexto">
+          <FormInput
+            placeholder="Ingrese su DNI"
+            value={dni}
+            onChange={(e) => setDni(e.target.value)}
+            className="recuadroInputs"
+            icono="address-card"
+          />
+          <FormInput
+            type="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="recuadroInputs"
+            icono="lock"
+          />
+          <Button
+            text={"Iniciar sesión"}
+            onClick={handleLogin}
+            className="botonClaro"
+          />
 
-
-      <div className="alineacion-texto">
-        <p className="texto">
-          <a className="texto" href="https://ar.pinterest.com/pin/369084131975098694/" target="_blank">¿Olvidaste la contraseña?</a>
-        </p>
-        <p id="espacio">¿No tenés cuenta?</p>
-        <Button text="Registrate" onClick={handleRegister} className="botonClaro"/>
+          <div className="alineacion-texto">
+            <p className="texto">
+              <a
+                className="texto"
+                href="https://ar.pinterest.com/pin/369084131975098694/"
+                target="_blank"
+              >
+                ¿Olvidaste la contraseña?
+              </a>
+            </p>
+            <p id="espacio">¿No tenés cuenta?</p>
+            <Button
+              text="Registrate"
+              onClick={handleRegister}
+              className="botonClaro"
+            />
+          </div>
         </div>
-      
-    </div>
 
-    <div>
-    <img src="../assets/unahur-logo-figma-sf.png" className="unahur-logo" alt="Logo UNAHUR"/> 
-    </div>
-    </div>
+        <div>
+          <img
+            src="../assets/unahur-logo-figma-sf.png"
+            className="unahur-logo"
+            alt="Logo UNAHUR"
+          />
+        </div>
+      </div>
+
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={3000}
+        onClose={() => setOpenSnackbar(false)}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          margin: "auto",
+        }}
+      >
+        <Alert
+          onClose={() => setOpenSnackbar(false)}
+          severity="error"
+          sx={{ width: "50%" }}
+        >
+          <ul>
+            <li>{error}</li>
+          </ul>
+        </Alert>
+      </Snackbar>
     </>
   );
 }
