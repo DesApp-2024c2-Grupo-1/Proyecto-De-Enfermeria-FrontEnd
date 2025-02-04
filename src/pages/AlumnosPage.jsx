509720698dcef911965d018cc8236029e2883b78
maia-ab <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2025-02-04 20:27:40 -0300

    arrgle listas en alumnosPage y perfilAlumno

--- src/pages/AlumnosPage.jsx
+++ src/pages/AlumnosPage.jsx
@@ -3,19 +3,22 @@ import Busqueda from "../components/Busqueda";
 import { useNavigate } from "react-router-dom";
 import Lista from "../components/Lista";
 import { getAllAlumnos } from "../services/AlumnoService";
+import { Stack, Box } from "@mui/material";
 
 export function AlumnosPage() {
-  const [searchTerm, setSearchTerm] = useState(""); 
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
 
-  const keys = ["nombre", "apellido", "dni"]
+  const keys = ["nombre", "apellido", "dni"];
   const [alumnos, setAlumnos] = useState([]);
-  const listaFiltrada = searchTerm.length >= 7 ? alumnos.filter((alumno) => 
-    String(alumno.dni).includes(searchTerm)) : alumnos;
+  const listaFiltrada =
+    searchTerm.length >= 7
+      ? alumnos.filter((alumno) => String(alumno.dni).includes(searchTerm))
+      : alumnos;
 
   const fetchAlumnos = async () => {
     const data = await getAllAlumnos();
-    setAlumnos(data)
+    setAlumnos(data);
   };
 
   useEffect(() => {
@@ -26,23 +29,32 @@ export function AlumnosPage() {
     navigate(`/perfilAlumno/${dni}`);
   };
 
-
-  return ( 
+  return (
     <>
-      <div className="registroEvaluacionesContainer">
+      <Stack sx={{ justifyContent: "center", alignItems: "center" }}>
         <h1>Alumnos</h1>
-        <div className="registroEvaluaciones">
-          <Busqueda 
-            placeholder="Buscar por DNI..."
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <Lista
-            lista={listaFiltrada}
-            keys={keys}
-            buttonOnClick={handleNavigate}
-          />
-        </div>
-      </div>
+        <Stack
+          sx={{
+            width: "80%",
+            justifyContent: "center",
+          }}
+          spacing={2}
+        >
+          <Box>
+            <Busqueda
+              placeholder="Buscar por DNI..."
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </Box>
+          <Box>
+            <Lista
+              lista={listaFiltrada}
+              keys={keys}
+              buttonOnClick={handleNavigate}
+            />
+          </Box>
+        </Stack>
+      </Stack>
     </>
   );
 }
