ab3e55fb1f384c34eef0ed0c5224c381bc51b51c
maia-ab <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2025-02-16 22:10:25 -0300

    implemente con box y centre el icono

--- src/components/Pregunta.jsx
+++ src/components/Pregunta.jsx
@@ -11,31 +11,22 @@ export function Pregunta({ pregunta, puntaje, respuesta, disabled, onChange }) {
         borderBottom: "1px solid black",
       }}
     >
-      <Box sx={{ maxWidth: "60%", textAlign: "center" }}>
-        <p className="alineacionIzquierda">{pregunta}</p>
+      <Box sx={{ maxWidth: "60%", textAlign: "justify" }}>
+        <p>{pregunta}</p>
       </Box>
 
       <Stack direction={"row"} spacing={2.5}>
         <p style={{ marginRight: 50 }}>{puntaje}</p>
 
-        <input
-          type="radio"
-          id={`si-${pregunta}`}
-          name={`respuesta-${pregunta}`}
-          value="Si"
-          disabled={disabled}
-          checked={respuesta === true}
-          onChange={() => onChange(true)}
-          style={{ display: "none" }}
-        />
-        <label
-          htmlFor={`si-${pregunta}`}
-          style={{
-            display: "inline-block",
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            border: "2px solid black",
+            borderRadius: "100px",
             width: "25px",
             height: "25px",
-            borderRadius: "50%",
-            border: "2px solid black",
             textAlign: "center",
             lineHeight: "23px",
             cursor: disabled ? "not-allowed" : "pointer",
@@ -43,27 +34,29 @@ export function Pregunta({ pregunta, puntaje, respuesta, disabled, onChange }) {
             color: respuesta === true ? "white" : "black",
           }}
         >
-          ✓
-        </label>
-
-        <input
-          type="radio"
-          id={`no-${pregunta}`}
-          name={`respuesta-${pregunta}`}
-          value="No"
-          disabled={disabled}
-          checked={respuesta === false}
-          onChange={() => onChange(false)}
-          style={{ display: "none" }}
-        />
-        <label
-          htmlFor={`no-${pregunta}`}
-          style={{
-            display: "inline-block",
+          <input
+            type="radio"
+            id={`si-${pregunta}`}
+            name={`respuesta-${pregunta}`}
+            value="Si"
+            disabled={disabled}
+            checked={respuesta === true}
+            onChange={() => onChange(true)}
+            style={{ display: "none" }}
+          />
+          <label htmlFor={`si-${pregunta}`}>
+            ✓
+          </label>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            border: "2px solid black",
+            borderRadius: "100px",
             width: "25px",
             height: "25px",
-            borderRadius: "50%",
-            border: "2px solid black",
             textAlign: "center",
             lineHeight: "23px",
             cursor: disabled ? "not-allowed" : "pointer",
@@ -71,22 +64,23 @@ export function Pregunta({ pregunta, puntaje, respuesta, disabled, onChange }) {
             color: respuesta === false ? "white" : "black",
           }}
         >
-          ✗
-        </label>
+          <input
+            type="radio"
+            id={`no-${pregunta}`}
+            name={`respuesta-${pregunta}`}
+            value="No"
+            disabled={disabled}
+            checked={respuesta === false}
+            onChange={() => onChange(false)}
+            style={{ display: "none" }}
+          />
+          <label htmlFor={`no-${pregunta}`}>
+            ✗
+          </label>
+        </Box>
       </Stack>
     </Stack>
   );
 }
 
 
-/* contexto de porq los estilos están aca y no en una clase css: 
-inicialmente lo arranqué como clase css, pero como lo encare x el lado de basarme en la respuesta (o sea si está checked) para q cambiara de color, tuve q hacerlo 
-acá individualmente para cada uno ya q las condiciones de "checked" son distintas.
-
-
-el display none del input esconde el input incial de html. dps con html for y la id q le pusimos, se sobrepone la label con su estilo.
-
-lo del cursor "not allowed" lo puse para q sea mas intuitivo cuando una evaluación está cerrada, que no te deja interactuar con las respuestas. igual si crees
-q está demas le pones "default" donde dice not allowed y queda normal al hovear sobre el input, el tema es q el ternario tendría q estar para q si el input
-no está disabled se convierta en "pointer" dándote a entender q podés clickear.
-*/
