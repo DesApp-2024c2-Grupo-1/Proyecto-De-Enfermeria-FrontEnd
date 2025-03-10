18d7f13f1146f95bceb6334e550e2f8d0901022f
maia-ab <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2025-03-09 23:22:24 -0300

    cambios de implementacion y estilo a listItem

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -1,18 +1,15 @@
 import ListItem from "./ListItem";
 
-function Lista({ titulo,  lista, keys, buttonOnClick, className}) {
+function Lista({ titulo, lista, keys, buttonOnClick, className }) {
   return (
     <div className={className}>
       <h2>{titulo}</h2>
-      {lista.map((item, index) => (
+      {lista.map((item) => (
         <ListItem
-          key={index}
-          textoUno={item[keys[0]]} 
-          textoDos={item[keys[1]]} 
-          textoTres={item[keys[2]]} 
-          buttonOnClick={() => buttonOnClick(item.dni)} 
+          textos={keys.map((key) => item[key])}
+          buttonOnClick={() => buttonOnClick(item.dni)}
         />
-      ))} 
+      ))}
     </div>
   );
 }
