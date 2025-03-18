00b2b673f8fa7c1baa754e0816a5ea203bb43689
maia-ab <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2025-03-18 15:45:09 -0300

    añadi keys

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -1,11 +1,19 @@
 import ListItem from "./ListItem";
 
-function Lista({ titulo, lista, keys, buttonOnClick, className, paramOnClick }) {
+function Lista({
+  titulo,
+  lista,
+  keys,
+  buttonOnClick,
+  className,
+  paramOnClick,
+}) {
   return (
     <div className={className}>
       <h2>{titulo}</h2>
-      {lista.map((item) => (
+      {lista.map((item, index) => (
         <ListItem
+          key={item.id || index}
           textos={keys.map((key) => item[key])}
           buttonOnClick={() => buttonOnClick(item[paramOnClick])}
         />
