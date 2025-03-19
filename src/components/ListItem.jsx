b4b9740e7a5e6bcf002d17233610c23966590a6f
maximilianoalmada <maxialma03@gmail.com>
2025-03-19 13:15:26 -0300

    cambios en la version responsive de listitem

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,6 +1,9 @@
-import { Stack, Button, Box } from "@mui/material";
+import { Stack, Button, Box, useMediaQuery } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 
 function ListItem({ textos, buttonOnClick }) {
+  const theme = createTheme();
+  const xs = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
     <>
@@ -12,7 +15,7 @@ function ListItem({ textos, buttonOnClick }) {
           my: "20px",
           px: "20px",
           py: {xs: "10px", sm: "0px"},
-          borderRadius: "100px",
+          borderRadius: {xs: "15px", sm: "100px"},
           boxShadow: "3px 3px 5px rgba(0, 0, 0, 0.3)",
           display: "flex",
           alignItems: "center",
@@ -26,9 +29,23 @@ function ListItem({ textos, buttonOnClick }) {
             </div>
           ))}
         </Stack>
-        <Button
+        { xs ? <Button
           sx={{
-            px: { xs: "80px", sm: "20px", md: "50px" },
+            px: { xs: "20px", sm: "20px", md: "50px" },
+            borderRadius: "100px",
+            boxShadow: "3px 3px 5px rgba(0, 0, 0, 0.3)",
+            backgroundColor: "#31614b",
+            "&:hover": {
+              backgroundColor: "#234637",
+            },
+          }}
+          variant="contained"
+          onClick={buttonOnClick}
+        >
+          <i class="fa-regular fa-eye"></i>
+        </Button> : <Button
+          sx={{
+            px: { xs: "20px", sm: "20px", md: "50px" },
             borderRadius: "100px",
             boxShadow: "3px 3px 5px rgba(0, 0, 0, 0.3)",
             backgroundColor: "#31614b",
@@ -40,7 +57,7 @@ function ListItem({ textos, buttonOnClick }) {
           onClick={buttonOnClick}
         >
           Ver
-        </Button>
+        </Button>}       
       </Stack>
     </>
   );
