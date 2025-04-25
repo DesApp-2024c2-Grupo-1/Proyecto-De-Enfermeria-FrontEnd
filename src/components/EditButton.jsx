0a4f74dc7ed48efcac53eff77adddc522b41d233
Maia <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2025-04-24 22:40:13 -0300

    button dropdown

--- src/components/EditButton.jsx
+++ src/components/EditButton.jsx
@@ -1,27 +1,76 @@
-import { Fab, Zoom, Button } from "@mui/material";
-import { useScrollTrigger } from "@mui/material";
+import {
+  Button,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+
+import { useState } from "react";
 
 function EditButton() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <>
+    <div>
       <Button
         sx={{
+          backgroundColor: "#31614b",
+          color: "white",
+          borderRadius: "100%",
           minHeight: 0,
           minWidth: 0,
           height: "30px",
           width: "30px",
-          marginTop: "1px",
-          borderRadius: "100%",
-          boxShadow: "3px 3px 5px rgba(0, 0, 0, 0.3)",
-          backgroundColor: "#31614b",
+
           "&:hover": {
-            backgroundColor: "#234637",
+            backgroundColor: "#3e7d5d",
           },
         }}
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
       >
-        <i class="fa-solid fa-gear" style={{ color: "white" }}></i>
+        <i className="fa-solid fa-gear"></i>
       </Button>
-    </>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem>
+          <ListItemIcon>
+            <i className="fa-solid fa-edit"></i>
+          </ListItemIcon>
+          <ListItemText>Editar</ListItemText>
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <i className="fa-solid fa-clock"></i>
+          </ListItemIcon>
+          <ListItemText>Historial</ListItemText>
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <i className="fa-solid fa-trash"></i>
+          </ListItemIcon>
+          <ListItemText>Eliminar</ListItemText>
+        </MenuItem>
+      </Menu>
+    </div>
   );
 }
 
