040e28789d8d06f15419729c480d5c679996c5f2
Maia <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2025-05-16 22:58:06 -0300

    implementamos desahibitar evaluacion

--- src/services/EvaluacionService.js
+++ src/services/EvaluacionService.js
@@ -19,3 +19,8 @@ export const putEvaluacionYPreguntas = async (evaluacionYPreguntasData, id) => {
     const response = await axios.put(`http://localhost:3000/evaluacion/modificar-evaluacion/${id}`, evaluacionYPreguntasData);
     return response.data;
 };
+
+export const deshabilitarEvaluacion = async (id) => {
+    const response = await axios.put(`http://localhost:3000/evaluacion/${id}`);
+    return response.data;
+};
