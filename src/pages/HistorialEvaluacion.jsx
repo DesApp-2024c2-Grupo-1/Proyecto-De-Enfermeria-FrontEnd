49853289227da7b93189840d8a40630e5cbb1697
Maia <maiaagustina.barrionuevo@estudiantes.unahur.edu.ar>
2025-06-26 23:47:33 -0300

    modificaciones

--- src/pages/HistorialEvaluacion.jsx
+++ src/pages/HistorialEvaluacion.jsx
@@ -1,30 +1,52 @@
 import { useParams } from "react-router-dom";
-import { Button, Stack, TextField, Grid } from "@mui/material";
-
+import { Button, Stack, TextField, Grid, Pagination, useMediaQuery } from "@mui/material";
+import ListHeader from "../components/Header";
+import Lista from "../components/Lista";
+import { createTheme } from "@mui/material/styles";
 import Carpeta from "../components/Carpeta";
 import Busqueda from "../components/Busqueda";
 import IrArribaBoton from "../components/irArribaBoton";
 import { useEffect, useState } from "react";
 import { getAllVersionesDeUnModelo } from "../services/EvaluacionService";
 
-
 export function HistorialEvaluacion() {
   const { id } = useParams();
   const [searchTerm, setSearchTerm] = useState("");
   const [carpetas, setCarpetas] = useState([]);
+  const keys = ["titulo", "version", "fechaModificacion"];
+    const theme = createTheme();
+  const xs = useMediaQuery(theme.breakpoints.down("sm"));
+  
+  
   const listaFiltrada = carpetas.filter((carpeta) =>
     carpeta.titulo.toLowerCase().includes(searchTerm)
   );
 
+  const textosHeader = [
+    { key: "titulo", value: "Titulo" },
+    { key: "version", value: "Version" },
+    ,
+  ];
 
-    const fetchCarpetas = async () => {
-      const data = await getAllVersionesDeUnModelo(id);
-      setCarpetas(data);
-    };
-  
-    useEffect(() => {
-      fetchCarpetas();
-    }, []);
+    const handleNavigate = (id) => {
+    navigate(`/perfilAlumno/${id}`, {
+      state: {
+        alumnoNombre: alumnos.find((alumno) => alumno.id === id).nombre,
+        alumnoApellido: alumnos.find((alumno) => alumno.id === id).apellido,
+      },
+    });
+  };
+
+
+  const fetchCarpetas = async () => {
+    const data = await getAllVersionesDeUnModelo(id);
+    setCarpetas(data);
+  };
+
+  useEffect(() => {
+    fetchCarpetas();
+    console.log(carpetas);
+  }, []);
 
   return (
     <>
@@ -39,6 +61,20 @@ export function HistorialEvaluacion() {
           margin={"0 0 60px 0"}
           onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
         />
+                <Stack
+          sx={{
+            width: xs ? "75%" : "70%",
+          }}
+        >
+        <ListHeader key={keys} textos={textosHeader} />
+        <Lista
+          lista={listaFiltrada}
+          keys={keys}
+          buttonOnClick={handleNavigate}
+          paramOnClick="id"
+        />
+        </Stack>
+        {/*
         <Grid container spacing={10} sx={{ pb: "60px" }}>
           {listaFiltrada.map((evaluacion, index) => (
             <Grid item xs={12} sm={6} md={4} key={index}>
@@ -46,6 +82,7 @@ export function HistorialEvaluacion() {
             </Grid>
           ))}
         </Grid>
+        */}
       </Stack>
     </>
   );
